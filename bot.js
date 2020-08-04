80dc955f2f6b620d8b0147d9dfe1f4e01217157c
Jeremy <jveto91@gmail.com>
2020-08-03 20:44:50 -0500

    3 games add/remove finished

--- bot.js
+++ bot.js
@@ -1,16 +1,118 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//const guild = new Discord.Guild(client, null);
+var guild = client.guilds.cache;
+var curGuild; 
+
 
 client.on('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(guild)
+    curGuild = guild.find(name => name.name == 'Bubbles');
+    console.log(curGuild.id);
+    var roles = curGuild.roles.cache.forEach(x => {
+        console.log(x);
+    })
+    guild.forEach(x => {
+        //console.log(x.roles);
+    })
 })
 
+
 client.on('message', msg =>{
-    if(msg.content == 'Ping'){
-        msg.reply('Pong!');
-        msg.channel.send('Pong again!');
+    if(msg.author.username == "BubbleBuddy") return;
+    //console.log(msg);
+    if(msg.content[0] == '!'){
+        var args = msg.content.substring(1, msg.content.length).split(" ");
+        console.log("args: ", args);
+        switch (args[0].toUpperCase()){
+            case "JOIN":
+                if(args.length < 2) return;
+                args.splice(0,1);
+                args = args.join(" ");
+                console.log(args);
+                switch(args.toUpperCase()){
+                    case "APEX":
+                        AddGameRole("APEX", msg.author.id, msg, "Apex Squad");
+                        break;
+                    case "PUMMEL PARTY":
+                        AddGameRole("PUMMEL PARTY", msg.author.id, msg, "Pummel Party Crew");
+                        break;
+                    case "VALORANT":
+                        AddGameRole("VALORANT", msg.author.id, msg, "Valorant Gang");
+                        break;
+                }
+                break;
+            case "LEAVE":
+                if(args.length < 2) return;
+                args.splice(0,1);
+                args = args.join(" ");
+                console.log(args);
+                switch(args.toUpperCase()){
+                    case "APEX":
+                        RemoveGameRole("APEX", msg.author.id, msg, "Apex Squad");
+                        break;
+                    case "PUMMEL PARTY":
+                        RemoveGameRole("PUMMEL PARTY", msg.author.id, msg, "Pummel Party Crew");
+                        break;
+                    case "VALORANT":
+                        RemoveGameRole("VALORANT", msg.author.id, msg, "Valorant Gang");
+                        break;
+                }
+                break;
+        }
     }
 })
+function RemoveGameRole(gameName, userId, bot, roleName){
+    console.log("Attempting to remove");
+    var role = curGuild.roles.cache.find(role => role.name == roleName);             
+    const promise = curGuild.members.fetch(userId);;
+    promise.then(suc, err);
+    function suc(user){
+        attemptToRemoveRole(user, role);
+    }
+    function err(err){
+        console.log("err: ", err);
+    }
+
+    function attemptToRemoveRole(user, role){
+        if(user._roles.includes(role.id)){
+            user.roles.remove(role);
+            console.log("Removed");
+            bot.channel.send(`You have sucessfully left <@&${role.id}>!`);
+        }
+        else{
+            console.log("NonMember");
+            bot.channel.send(`You don't even go here!`);
+        }
+    };
+}
+
+function AddGameRole(gameName, userId, bot, roleName){
+    var role = curGuild.roles.cache.find(role => role.name == roleName);             
+    const promise = curGuild.members.fetch(userId);;
+    promise.then(suc, err);
+    function suc(user){
+        attemptToAddRole(user, role);
+    }
+    function err(err){
+        console.log("err: ", err);
+    }
+
+
+    function attemptToAddRole(user, role){
+        if(user._roles.includes(role.id)){
+            console.log("AlreadyMember");
+            bot.channel.send("You are already a member of this group you dork!");
+        }
+        else{
+            user.roles.add(role);
+            console.log("Added");
+            bot.channel.send(`Another member has joined <@&${role.id}>`);
+        }
+    };
+}
+
 
 client.login('NzM5NjcwODUwNTQ2MDQwOTE0.Xyd2KQ.IYYXCP9shmRnUSNMFrxskjNoDuQ');
 
